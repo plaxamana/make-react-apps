e1b9ba12f50d08973b1f3d106d43437f4d8069ea
Philip Laxamana <philip.j.laxamana@gmail.com>
2022-08-06 21:38:30 -0400

    added analog sound to pomodoro timer

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import useSound from 'use-sound';
+import analogAlarmSonud  from './analog_alarm.wav';
 import './App.css';
 
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
 export default function App() {
+  const initialTime = 10;
+
+  const [title, setTitle] = useState('Let the countdown begin!');
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+  const [isRunning, setIsRunning] = useState(false);
+  const [playAlarm] = useSound(analogAlarmSonud, { volume: 0.25 });
+  const intervalRef = useRef(null);
+
+  function startTimer() {
+    // don't set another time if timer is active
+    if (intervalRef.current !== null) return;
+    
+    setTitle(`You're doing great!`);
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        resetTimer();
+        playAlarm();
+        return 0;
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current === null) return;
+
+    setIsRunning(false);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up!');
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setIsRunning(false);
+    setTitle('Ready for another round?');
+    setTimeLeft(initialTime);
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        {timeLeft < initialTime && (
+          <button onClick={resetTimer}>Reset</button>
+        )}
       </div>
     </div>
   );
